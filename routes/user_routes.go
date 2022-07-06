58dbc14978f76ef5c44e1c29b853c3c89504b475
Ernesto <eojeda@gmx.es>
2022-07-06 09:03:29 -0400

    ...

--- routes/user_routes.go
+++ routes/user_routes.go
@@ -6,7 +6,6 @@ import (
 )
 
 func UserRoute(app *fiber.App) {
-	//app.Get("/", controllers.HelloWorld)
 	app.Post("/users", Authenticate, controllers.CreateUser)
 	app.Post("/users/flat", controllers.CreateUser2)
 	app.Get("/users/:userId", Authenticate, controllers.GetAUser)
